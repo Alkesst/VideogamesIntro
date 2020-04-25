d838e08a82c640951392cb3889cf41ac3e8c45fb
Alejandro Garau Madrigal <alegarmad@uma.es>
2020-04-25 20:16:50 +0200

    Behaviour of one npc already done

--- Assets/Scripts/NPC Scripts/RampaBehaviour.cs
+++ Assets/Scripts/NPC Scripts/RampaBehaviour.cs
@@ -3,20 +3,26 @@ using UnityEngine.AI;
 
 public class RampaBehaviour : MonoBehaviour
 {
-    private enum States { patrullando, combatiendo }
+    private enum States { andando, buscando, combatiendo }
     private readonly float AGENT_SPEED_PATROL = 4f;
     private readonly float AGENT_SPEED_FIGHT = 4f;
+    private readonly Vector3 LOOKUP_INITIAL = new Vector3(25, 1, -20);
+    private readonly Vector3 LOOKUP_FINAL = new Vector3(-25, 1, -20);
+    private readonly float LOOKUP_SPEED = 10f;
+    private float LOOKUP_LENGTH;
 
     public NavMeshAgent agent;
     private States state;
     public GameObject[] path;
     private int current;
     private GameObject player;
+    private float currentTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        state = States.patrullando;
+        LOOKUP_LENGTH = Vector3.Distance(LOOKUP_INITIAL, LOOKUP_FINAL); 
+        state = States.andando;
         if (agent == null) agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(path[0].transform.position);
     }
@@ -24,19 +30,35 @@ public class RampaBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(state == States.patrullando)
+        if(state == States.andando)
         {
             if(agent.remainingDistance <= agent.stoppingDistance)
             {
                 current++;
                 if (current >= path.Length) current = 0;
                 agent.speed = AGENT_SPEED_PATROL;
-                agent.SetDestination(path[current].transform.position);
+                state = States.buscando;
+                currentTime = Time.time;
             } else if (state == States.combatiendo)
             {
                 transform.LookAt(player.transform);
                 agent.speed = AGENT_SPEED_FIGHT;
                 agent.SetDestination(player.transform.position);
+            }
+        } else if (state == States.buscando)
+        {
+            float distCovered = (Time.time - currentTime) * LOOKUP_SPEED;
+            float fractionOfJourney = distCovered / LOOKUP_LENGTH;
+            if (current == 0) {
+                transform.LookAt(Vector3.Lerp(LOOKUP_INITIAL + new Vector3(0, 0, 50), LOOKUP_FINAL + new Vector3(0, 0, 50), fractionOfJourney));
+            } else
+            {
+                transform.LookAt(Vector3.Lerp(LOOKUP_INITIAL, LOOKUP_FINAL, fractionOfJourney));
+            }
+            if(fractionOfJourney >= 1)
+            {
+                agent.SetDestination(path[current].transform.position);
+                state = States.andando;
             }
         }
         
