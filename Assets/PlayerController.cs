063c9e9f70a86f3b5cc454ad0ba054f1b14355b1
Alejandro Garau Madrigal <alegarmad@uma.es>
2020-04-25 18:29:20 +0200

    Movement and camera done

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidadMovimiento;
+    public float velocidadRotacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Rotate(new Vector3(0, 1, 0), horizontal * velocidadRotacion);
+        transform.Translate(Vector3.forward * vertical * velocidadMovimiento * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private float deltaY, deltaZ, deltaAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        deltaY = 3f;
+        deltaZ = -2.5f;
+        deltaAngle = 20;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, deltaY, 0) + deltaZ * player.transform.forward;
+        transform.rotation = player.transform.rotation;
+        transform.Rotate(new Vector3(1, 0, 0), deltaAngle);
+    }
+}
